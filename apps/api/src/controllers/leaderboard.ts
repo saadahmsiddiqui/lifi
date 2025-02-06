2f597ccf1eff0f59e705cf06f6b7e89c2f71acd4
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2025-02-06 19:55:37 +0100

    chore: minor changes

--- apps/api/src/controllers/leaderboard.ts
+++ apps/api/src/controllers/leaderboard.ts
@@ -17,7 +17,7 @@ export const getLeaderboard = async (
   reply: FastifyReply,
 ) => {
   const leaderboard = await redisGetLeaderboard();
-  return reply.send(leaderboard?.reverse());
+  return reply.send(leaderboard ?? []);
 };
 
 export default {
