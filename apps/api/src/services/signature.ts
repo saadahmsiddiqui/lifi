8a9b2bc0c4f1607f16437b5790efc9f3955047ad
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2025-02-06 01:54:14 +0100

    chore: improvement minor

--- apps/api/src/services/signature.ts
+++ apps/api/src/services/signature.ts
@@ -1,11 +1,10 @@
 import { verifyMessage } from "ethers";
 
 export function isValidSignature(signature: string, account: string) {
-  const message = "This is a LIFI Test";
   let isValid = true;
 
   try {
-    const signerAddress = verifyMessage(message, signature);
+    const signerAddress = verifyMessage(process.env.MESSAGE_TO_SIGN!, signature);
 
     if (signerAddress.toLowerCase() !== account.toLowerCase()) {
       isValid = false;
